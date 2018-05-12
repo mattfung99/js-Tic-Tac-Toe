260d4768bf89ce94a84f77595b4d8ae0fe14f2cf
unknown <official.matthiasfung@gmail.com>
2018-05-12 03:35:58 -0400

    Completion of SP-04

--- SpikesAndInvestigations/generateDivs.js
+++ SpikesAndInvestigations/generateDivs.js
@@ -1,8 +1,8 @@
 var attachClass,
-    flexContainer;
-var counter = 0;
-var idTracker = [];
-var numberOfBoxes = 9;
+    counterForXO = 0,
+    flexContainer,
+    idTracker = [],
+    numberOfBoxes = 9;
 
 flexContainer = document.getElementsByTagName("div")[0];
 attachClass = document.createAttribute("class");
@@ -10,19 +10,33 @@ attachClass.value = "flex-container";
 flexContainer.setAttributeNode(attachClass);
 
 function createBoxes() {
-    for(var i = 1; i <= numberOfBoxes; i++) {
+    for (var i = 1; i <= numberOfBoxes; i++) {
         generateBoxes();
     }
     generateCounterDisplay();
 }
 
 function generateBoxes() {
-    var box = document.createElement("div");
-
+    var box = document.createElement("button");
     box.id = "box" + idTracker.length;
-    box.style = "background: beige; height: 150px; margin-bottom: 20px; width: 150px; text-align: center; width: 150px";
+    box.style = "background: beige; cursor: pointer; height: 150px; margin-bottom: 20px; outline: 0; text-align: center; width: 150px";
     box.addEventListener("click", function () {
         mouseClickCounter();
+        console.log(box.id);
+        switch (counterForXO) {
+            case 1:
+                document.getElementById(box.id).innerHTML = "X";
+                document.getElementById(box.id).disabled = true;
+                box.style.cursor = "default";
+                break;
+
+            case 2:
+                document.getElementById(box.id).innerHTML = "O";
+                document.getElementById(box.id).disabled = true;
+                box.style.cursor = "default";
+                counterForXO = 0;
+                break;
+        }
     });
 
     document.getElementById("flex-container").appendChild(box);
@@ -42,6 +56,6 @@ function generateCounterDisplay() {
 }
 
 function mouseClickCounter() {
-    counter++;
-    document.getElementById("display-counter").innerHTML = counter;
+    counterForXO++;
+    document.getElementById("display-counter").innerHTML = counterForXO;
 }
