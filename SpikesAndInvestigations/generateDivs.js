51396949ea745c51a9314ae9aa042ebbccad8d77
unknown <official.matthiasfung@gmail.com>
2018-05-13 01:53:25 -0400

    Completion of SP-05

--- SpikesAndInvestigations/generateDivs.js
+++ SpikesAndInvestigations/generateDivs.js
@@ -1,30 +1,31 @@
 var attachClass,
+    boxesClicked = 0,
     counterForXO = 0,
     flexContainer,
     idTracker = [],
     numberOfBoxes = 9;
 
-var trackerO = {
-    oHorizontal123: 0,
-    oHorizontal456: 0,
-    oHorizontal789: 0,
-    oVertical123: 0,
-    oVertical456: 0,
-    oyVertical789: 0,
-    oDiagonal159: 0,
-    oDiagonal357: 0,
-};
+var trackerO = [
+    oHorizontal123 = 0,
+    oHorizontal456 = 0,
+    oHorizontal789 = 0,
+    oVertical147 = 0,
+    oVertical258 = 0,
+    oVertical369 = 0,
+    oDiagonal159 = 0,
+    oDiagonal357 = 0,
+];
 
-var trackerX = {
-    xHorizontal123: 0,
-    xHorizontal456: 0,
-    xHorizontal789: 0,
-    xVertical123: 0,
-    xVertical456: 0,
-    xVertical789: 0,
-    xDiagonal159: 0,
-    xDiagonal357: 0,
-};
+var trackerX = [
+    xHorizontal123 = 0,
+    xHorizontal456 = 0,
+    xHorizontal789 = 0,
+    xVertical147 = 0,
+    xVertical258 = 0,
+    xVertical369 = 0,
+    xDiagonal159 = 0,
+    xDiagonal357 = 0,
+];
 
 flexContainer = document.getElementsByTagName("div")[0];
 attachClass = document.createAttribute("class");
@@ -49,100 +50,106 @@ function generateBoxes() {
             case 1:
                 document.getElementById(box.id).innerHTML = "X";
                 document.getElementById(box.id).disabled = true;
+                boxesClicked++;
+                // console.log("Number of Boxes Clicked " + boxesClicked);
                 switch (box.id) {
                     case "box1":
-                        trackerX.xHorizontal123++;
-                        trackerX.xVertical123++;
-                        trackerX.xDiagonal159++;
+                        trackerX[0]++;
+                        trackerX[3]++;
+                        trackerX[6]++;
                         break;
                     case "box2":
-                        trackerX.xHorizontal123++;
-                        trackerX.xVertical123++;
+                        trackerX[0]++;
+                        trackerX[4]++;
                         break;
                     case "box3":
-                        trackerX.xHorizontal123++;
-                        trackerX.xVertical123++;
-                        trackerX.xDiagonal357++;
+                        trackerX[0]++;
+                        trackerX[5]++;
+                        trackerX[7]++;
                         break;
                     case "box4":
-                        trackerX.xHorizontal456++;
-                        trackerX.xVertical456++;
+                        trackerX[1]++;
+                        trackerX[3]++;
                         break;
                     case "box5":
-                        trackerX.xHorizontal456++;
-                        trackerX.xVertical456++;
-                        trackerX.xDiagonal159++;
-                        trackerX.xDiagonal357++;
+                        trackerX[1]++;
+                        trackerX[4]++;
+                        trackerX[6]++;
+                        trackerX[7]++;
                         break;
                     case "box6":
-                        trackerX.xHorizontal456++;
-                        trackerX.xVertical456++;
+                        trackerX[1]++;
+                        trackerX[5]++;
                         break;
                     case "box7":
-                        trackerX.xHorizontal789++;
-                        trackerX.xVertical789++;
-                        trackerX.xDiagonal357++;
+                        trackerX[2]++;
+                        trackerX[3]++;
+                        trackerX[7]++;
                         break;
                     case "box8":
-                        trackerX.xHorizontal789++;
-                        trackerX.xVertical789++;
+                        trackerX[2]++;
+                        trackerX[4]++;
                         break;
                     case "box9":
-                        trackerX.xHorizontal789++;
-                        trackerX.xVertical789++;
-                        trackerX.xDiagonal159++;
+                        trackerX[2]++;
+                        trackerX[5]++;
+                        trackerX[6]++;
                         break;
                 }
+                loopToCheckStatus();
                 box.style.cursor = "default";
                 break;
 
             case 2:
                 document.getElementById(box.id).innerHTML = "O";
                 document.getElementById(box.id).disabled = true;
+                boxesClicked++;
+                // console.log("Number of Boxes Clicked " + boxesClicked);
                 switch (box.id) {
                     case "box1":
-                        trackerO.oHorizontal123++;
-                        trackerO.oVertical123++;
-                        trackerO.oDiagonal159++;
+                        trackerO[0]++;
+                        trackerO[3]++;
+                        trackerO[6]++;
                         break;
                     case "box2":
-                        trackerO.oHorizontal123++;
-                        trackerO.oVertical123++;
+                        trackerO[0]++;
+                        trackerO[4]++;
                         break;
                     case "box3":
-                        trackerO.oHorizontal123++;
-                        trackerO.oVertical123++;
-                        trackerO.oDiagonal357++;
+                        trackerO[0]++;
+                        trackerO[5]++;
+                        trackerO[7]++;
                         break;
                     case "box4":
-                        trackerO.oHorizontal456++;
-                        trackerO.oVertical456++;
+                        trackerO[1]++;
+                        trackerO[3]++;
                         break;
                     case "box5":
-                        trackerO.oHorizontal456++;
-                        trackerO.oVertical456++;
-                        trackerO.oDiagonal159++;
-                        trackerO.oDiagonal357++;
+                        trackerO[1]++;
+                        trackerO[4]++;
+                        trackerO[6]++;
+                        trackerO[7]++;
                         break;
                     case "box6":
-                        trackerO.oHorizontal456++;
-                        trackerO.oVertical456++;
+                        trackerO[1]++;
+                        trackerO[5]++;
                         break;
                     case "box7":
-                        trackerO.oHorizontal789++;
-                        trackerO.oVertical789++;
-                        trackerO.oDiagonal357++;
+                        trackerO[2]++;
+                        trackerO[3]++;
+                        trackerO[7]++;
                         break;
                     case "box8":
-                        trackerO.oHorizontal789++;
-                        trackerO.oVertical789++;
+                        trackerO[2]++;
+                        trackerO[4]++;
                         break;
                     case "box9":
-                        trackerO.oHorizontal789++;
-                        trackerO.oVertical789++;
-                        trackerO.oDiagonal159++;
+                        trackerO[2]++;
+                        trackerO[5]++;
+                        trackerO[6]++;
                         break;
-                }
+                } 
+                loopToCheckStatus();
                 box.style.cursor = "default";
                 counterForXO = 0;
                 break;
@@ -173,4 +180,21 @@ function generateCounterDisplay() {
 function mouseClickCounter() {
     counterForXO++;
     document.getElementById("display-counter").innerHTML = counterForXO;
+}
+
+function loopToCheckStatus() {
+    for (var i in trackerX) {
+        console.log(trackerX[i]);
+        if (trackerX[i] == 3) {
+            document.getElementById("display-win").innerHTML = "X Wins";
+        }
+    }
+    for (var j in trackerO) {
+        console.log(trackerO[j]);
+        if (trackerO[j] == 3) {
+            document.getElementById("display-win").innerHTML = "O Wins";
+        }
+    }
+    console.log(trackerX);
+    console.log(trackerO);
 }
